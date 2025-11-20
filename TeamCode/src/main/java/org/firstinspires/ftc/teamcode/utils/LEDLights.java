dd3b650443a71fb75e73ff4695bffc4c18bc94a6
Vishnu <vishnu.ahya1@gmail.com>
2025-11-20 14:14:16 -0800

    I added a little on the color sensor classes and LED lights. I also worked on 3 ball auto for blue.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/LEDLights.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/LEDLights.java
@@ -13,23 +13,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LEDLights {
-    public static final double GREEN_WEIGHT = 0.500;
-    public static final double PURPLE_WEIGHT = 0.722;
     Servo LED;
+    String stringColor;
     Telemetry telemetry;
-
-
 
     public LEDLights(HardwareMap hw, String name) {
         LED = hw.get(Servo.class, name);
     }
 
     public LEDLights(HardwareMap hw, Telemetry t) {
-// Error        this(hw, "light");
+        this(hw, "light");
         telemetry = t;
     }
 
-    public void setColor(double color) {
+    public void setColor(double color, String theColor) {
         LED.setPosition(color);
+        stringColor = theColor;
     }
 }
