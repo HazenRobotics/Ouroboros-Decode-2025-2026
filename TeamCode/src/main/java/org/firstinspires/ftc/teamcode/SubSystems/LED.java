9d5417d815a750157185936ba33a69d982b5f16c
Vishnu <vishnu.ahya1@gmail.com>
2025-11-21 17:16:53 -0800

    Lights, ColorSensor, and shooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/LED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/LED.java
@@ -1,34 +1,34 @@
-package org.firstinspires.ftc.teamcode.SubSystems;
-
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
-import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-public class LED {
-    RevBlinkinLedDriver led;
-    private final String name = "led";
-
-    RevBlinkinLedDriver.BlinkinPattern[] lights = {
-            RevBlinkinLedDriver.BlinkinPattern.YELLOW,
-            RevBlinkinLedDriver.BlinkinPattern.VIOLET,
-            RevBlinkinLedDriver.BlinkinPattern.GREEN,
-    };
-
-    public LED(HardwareMap hw)
-    {
-        led = hw.get(RevBlinkinLedDriver.class, name);
-    }
-
-    public void reset()
-    {
-        led.setPattern(lights[0]);
-    }
-
-//    public void rangeToggle()
+//package org.firstinspires.ftc.teamcode.SubSystems;
+//
+//import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+//
+//import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//
+//public class LED {
+//    RevBlinkinLedDriver led;
+//    private final String name = "led";
+//
+//    RevBlinkinLedDriver.BlinkinPattern[] lights = {
+//            RevBlinkinLedDriver.BlinkinPattern.YELLOW,
+//            RevBlinkinLedDriver.BlinkinPattern.VIOLET,
+//            RevBlinkinLedDriver.BlinkinPattern.GREEN,
+//    };
+//
+//    public LED(HardwareMap hw)
 //    {
-//        //If Arraylist 1, switch to 2, and vice versa
-//        //Maybe use enums
-//        led.setPattern();
+//        led = hw.get(RevBlinkinLedDriver.class, name);
 //    }
-}
+//
+//    public void reset()
+//    {
+//        led.setPattern(lights[0]);
+//    }
+//
+////    public void rangeToggle()
+////    {
+////        //If Arraylist 1, switch to 2, and vice versa
+////        //Maybe use enums
+////        led.setPattern();
+////    }
+//}
