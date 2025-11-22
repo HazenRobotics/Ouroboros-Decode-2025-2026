abd913858e04a575f36d52fd4d873cdce20de71c
Vishnu <vishnu.ahya1@gmail.com>
2025-11-21 17:16:53 -0800

    Lights, ColorSensor, and shooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/StarterRobotTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/StarterRobotTeleOP.java
@@ -42,6 +42,7 @@ public class StarterRobotTeleOP extends LinearOpMode {
                 robot.load();
             }
 
+            // Driver 2 controllers made on the spot at 11/16 comp
             if(controller2.a.onPress()) {
                 robot.setLauncher();
             }
@@ -55,10 +56,16 @@ public class StarterRobotTeleOP extends LinearOpMode {
             if(controller2.y.onPress()){
                 robot.setFeederToZero();
             }
+            if(controller2.dpad_up.onPress()) {
+                robot.increaseV();
+            }
+            if(controller2.dpad_down.onPress()) {
+                robot.decreaseV();
+            }
 
             robot.updateReverseTransfer();
             robot.updateLoad();
-            robot.updateShooting();
+//            robot.updateShooting();
             robot.updateTransfer();
             controller1.update();
             controller2.update();
